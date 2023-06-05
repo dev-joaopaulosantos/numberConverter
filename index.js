1a79bfdf834c16d49f1e49256acb3a23b6a416f2
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-05 12:13:04 -0300

    refactor

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+const romanNumerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
 
 // função para converter números arábicos para romanos
 function arabicToRoman(num) {
     if (num < 1 || num > 3999) {
         return "Fora do intervalo permitido (1-3999)";
     }
-
-    const romanNumerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
 
     let roman = "";
     for (let key in romanNumerals) {
@@ -20,8 +19,7 @@ function arabicToRoman(num) {
 
 // função para converter números romanos para arábicos
 function romanToArabic(roman) {
-    const romanNumerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
-
+    
     let arabic = 0;
     for (let i = 0; i < roman.length; i++) {
         const currentSymbolValue = romanNumerals[roman[i]];
